ff4396ddc580aaa2d50f465b29a164615a549b73
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:29:11 -0600

    Add WorkExperience component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './components/Nav/Nav';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import WorkExperience from './components/WorkExperience/WorkExperience';
 import './App.css';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Nav />
       <Header />
       <About />
+      <WorkExperience />
     </section>
   );
 }
