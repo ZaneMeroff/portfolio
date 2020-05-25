e10595b9fe275cf5b000e0d5493f16b2705f57fd
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:29:44 -0600

    Add href for scroll

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,9 +6,9 @@ const placeholder = '';
 const Nav = () => {
   return (
     <nav className='nav-outer-container'>
-      <a href={placeholder} className='nav-links'><p>Home</p></a>
-      <a href={placeholder} className='nav-links'><p>About</p></a>
-      <a href={placeholder} className='nav-links'><p>Experience</p></a>
+      <a href='#header-section' className='nav-links'><p>Home</p></a>
+      <a href='#about-section' className='nav-links'><p>About</p></a>
+      <a href='#work-experience-section' className='nav-links'><p>Experience</p></a>
       <a href={placeholder} className='nav-links'><p>Projects</p></a>
       <a href={placeholder} className='nav-links'><p>Contact</p></a>
     </nav>
