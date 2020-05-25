ab3260435cc672c6b1c1ef98ad72352b0b267caa
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:46:36 -0600

    Add href link for projects

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
       <a href='#header-section' className='nav-links'><p>Home</p></a>
       <a href='#about-section' className='nav-links'><p>About</p></a>
       <a href='#work-experience-section' className='nav-links'><p>Experience</p></a>
-      <a href={placeholder} className='nav-links'><p>Projects</p></a>
+      <a href='#projects-section' className='nav-links'><p>Projects</p></a>
       <a href={placeholder} className='nav-links'><p>Contact</p></a>
     </nav>
   );
