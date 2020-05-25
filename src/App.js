53be6f576f394fe39e899c95b73ab17689c97ea6
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:51:20 -0600

    Add SimonGame component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './components/Nav/Nav';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import SimonGame from './components/SimonGame/SimonGame';
 import WorkExperience from './components/WorkExperience/WorkExperience';
 import Projects from './components/Projects/Projects';
 import './App.css';
@@ -12,6 +13,7 @@ const App = () => {
       <Nav />
       <Header />
       <About />
+      <SimonGame />
       <WorkExperience />
       <Projects />
     </section>
