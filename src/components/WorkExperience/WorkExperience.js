bd23d002d3d5a50d12cd34921d75ff443d791302
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:29:56 -0600

    Build component skeleton

---
+++ src/components/WorkExperience/WorkExperience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './WorkExperience.css';
+
+const WorkExperience = () => {
+
+  return (
+    <section id='work-experience-section' className='work-experience-outer-container'>
+      <h1>WORK EXPERIENCE</h1>
+    </section>
+  );
+
+}
+
+export default WorkExperience;
