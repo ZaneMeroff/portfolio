8a64c85d2f71c7bf0c9908d0e1ada35a32a530af
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 15:56:36 -0600

    Create dynamic smooth scroll function

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,50 +3,20 @@ import './Nav.css';
 
 const Nav = () => {
 
-  const scrollToHeader = () => {
-      document.querySelector('#header-section').scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-
-  const scrollToAbout = () => {
-      document.querySelector('#about-section').scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-
-  const scrollToSimonGame = () => {
-      document.querySelector('#simon-game-section').scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-
-  const scrollToWorkExperience = () => {
-      document.querySelector('#work-experience-section').scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-
-  const scrollToProjects = () => {
-      document.querySelector('#contact-section').scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-
-  const scrollToContact = () => {
-      document.querySelector('#projects-section').scrollIntoView({
+  const performSmoothScroll = sectionName => {
+      document.querySelector(`#${sectionName}-section`).scrollIntoView({
       behavior: 'smooth'
     });
   }
 
   return (
     <nav className='nav-outer-container'>
-      <p onClick={scrollToHeader} className='nav-links'>Home</p>
-      <p onClick={scrollToAbout} className='nav-links'>About</p>
-      <p onClick={scrollToSimonGame} className='nav-links'>Simon</p>
-      <p onClick={scrollToWorkExperience} className='nav-links'>Experience</p>
-      <p onClick={scrollToProjects} className='nav-links'>Projects</p>
-      <p onClick={scrollToContact} className='nav-links'>Contact</p>
+      <p onClick={() => performSmoothScroll('header')} className='nav-links'>Home</p>
+      <p onClick={() => performSmoothScroll('about')} className='nav-links'>About</p>
+      <p onClick={() => performSmoothScroll('simon-game')} className='nav-links'>Simon</p>
+      <p onClick={() => performSmoothScroll('work-experience')} className='nav-links'>Experience</p>
+      <p onClick={() => performSmoothScroll('projects')} className='nav-links'>Projects</p>
+      <p onClick={() => performSmoothScroll('contact')} className='nav-links'>Contact</p>
     </nav>
   );
 }
