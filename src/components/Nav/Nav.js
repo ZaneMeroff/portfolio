2124c512a3e3e456858f1acd73be4dad89855751
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:21:55 -0600

    Add uppercase CSS rule

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,11 +6,11 @@ const placeholder = '';
 const Nav = () => {
   return (
     <nav className='nav-outer-container'>
-      <a href={placeholder} className='nav-links'><p>HOME</p></a>
-      <a href={placeholder} className='nav-links'><p>ABOUT</p></a>
-      <a href={placeholder} className='nav-links'><p>EXPERIENCE</p></a>
-      <a href={placeholder} className='nav-links'><p>PROJECTS</p></a>
-      <a href={placeholder} className='nav-links'><p>CONTACT</p></a>
+      <a href={placeholder} className='nav-links'><p>Home</p></a>
+      <a href={placeholder} className='nav-links'><p>About</p></a>
+      <a href={placeholder} className='nav-links'><p>Experience</p></a>
+      <a href={placeholder} className='nav-links'><p>Projects</p></a>
+      <a href={placeholder} className='nav-links'><p>Contact</p></a>
     </nav>
   );
 }
