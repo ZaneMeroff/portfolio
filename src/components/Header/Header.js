2cfa5fe778e3ee918c57cc853cf514986e7ff92c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 13:44:41 -0600

    Add links to social accounts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './Header.css';
 import gitHubIcon from '../../images/github_icon.png';
 import twitterIcon from '../../images/twitter_icon.png';
 import linkedInIcon from '../../images/linkedin_icon.png';
-import './Header.css';
+
+const gitHubURL = 'https://github.com/ZaneMeroff';
+const linkedInURL = 'https://www.linkedin.com/in/zachary-nemeroff';
+const twitterURL = 'https://twitter.com/zanemeroff';
 
 const Header = () => {
   return (
     <section className='header-outer-container'>
       <h1 className='title-name'>ZACHARY NEMEROFF</h1>
       <h3 className='title-industry'>Software Developer</h3>
-      <img src={gitHubIcon} className='social-icons'/>
-      <img src={linkedInIcon} className='social-icons'/>
-      <img src={twitterIcon} className='social-icons'/>
+      <a href={gitHubURL} target="_blank"><img src={gitHubIcon} className='social-icons'/></a>
+      <a href={linkedInURL} target="_blank"><img src={linkedInIcon} className='social-icons'/></a>
+      <a href={twitterURL} target="_blank"><img src={twitterIcon} className='social-icons'/></a>
     </section>
   );
 }
