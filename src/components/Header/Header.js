8c52938ae7d2d375b4b7e447276c352ccf33cfe7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 13:26:46 -0600

    Import icon images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import gitHubIcon from '../../images/github_icon.png';
+import twitterIcon from '../../images/twitter_icon.png';
+import linkedInIcon from '../../images/linkedin_icon.png';
 import './Header.css';
 
 const Header = () => {
@@ -6,6 +9,9 @@ const Header = () => {
     <section className='header-outer-container'>
       <h1 className='title-name'>ZACHARY NEMEROFF</h1>
       <h3 className='title-industry'>Software Developer</h3>
+      <img src={gitHubIcon} className='social-icons'/>
+      <img src={linkedInIcon} className='social-icons'/>
+      <img src={twitterIcon} className='social-icons'/>
     </section>
   );
 }
