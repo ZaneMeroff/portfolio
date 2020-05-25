5ace2c35cc8bfe2ca2887972ceb340b30abb96b7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:56:21 -0600

    Add contact component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import SimonGame from './components/SimonGame/SimonGame';
 import WorkExperience from './components/WorkExperience/WorkExperience';
 import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import './App.css';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <SimonGame />
       <WorkExperience />
       <Projects />
+      <Contact />
     </section>
   );
 }
