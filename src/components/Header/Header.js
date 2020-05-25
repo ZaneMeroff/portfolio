e178c73d7c27435ffe495e9e8ef80bd3b9f7cd17
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 12:32:54 -0600

    Add className

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <section className='header-outer-container'>
-      <h1>ZACHARY NEMEROFF</h1>
+      <h1 className='title-name'>ZACHARY NEMEROFF</h1>
       <h3>Software Developer</h3>
     </section>
   );
