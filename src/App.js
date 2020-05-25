47aabe9c9c44c4449234cd8a2274aa21002b9beb
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 14:21:22 -0600

    Add About component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
+import Header from './components/Header/Header';
+import About from './components/About/About';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <section>
       <Nav />
       <Header />
+      <About />
     </section>
   );
 }
