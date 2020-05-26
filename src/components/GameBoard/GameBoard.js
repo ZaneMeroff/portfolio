7ae35d0ba996bcccebfa31bea90875b2d2fe26d8
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-26 10:33:55 -0600

    Refactor spacing

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -82,63 +82,63 @@ const GameBoard = () => {
   }
 
   const makeColorFlash = i => {
-  setTimeout(() => {
-    if (i % 2) {
-      colorKey[randomNumArray[Math.floor(j)]](true)
-    } else {
-      colorKey[randomNumArray[Math.floor(j)]](false)
-    }
-    j += 0.5
-    if (i === (roundCount * 2)) {
-      setMyOrYours('YOUR');
-      setDisableButtons(false);
-    }
-  }, i * 800 );
-}
-
-const generate10randomNumbers = () => {
-  let randomArray = [];
-  for (let i = 0; i < 10; i++) {
-    randomArray.push(Math.floor(Math.random() * 4))
+    setTimeout(() => {
+      if (i % 2) {
+        colorKey[randomNumArray[Math.floor(j)]](true)
+      } else {
+        colorKey[randomNumArray[Math.floor(j)]](false)
+      }
+      j += 0.5
+      if (i === (roundCount * 2)) {
+        setMyOrYours('YOUR');
+        setDisableButtons(false);
+      }
+    }, i * 800 );
   }
-  return randomArray;
-}
 
-const handlePlayerResponse = colorNum => {
-  playerResponse.push(colorNum);
-  playerClickCounter++
-  setPlayerClickDisplay(playerClickDisplay + 1);
-  if (roundCount === playerClickCounter) {
-    validateAnswer();
+  const generate10randomNumbers = () => {
+    let randomArray = [];
+    for (let i = 0; i < 10; i++) {
+      randomArray.push(Math.floor(Math.random() * 4))
+    }
+    return randomArray;
   }
-}
+
+  const handlePlayerResponse = colorNum => {
+    playerResponse.push(colorNum);
+    playerClickCounter++
+    setPlayerClickDisplay(playerClickDisplay + 1);
+    if (roundCount === playerClickCounter) {
+      validateAnswer();
+    }
+  }
 
   return (
     <section>
-    <ScoreBoard roundCount={roundCount} startGame={startGame} playerClickDisplay={playerClickDisplay} randomNumArray={randomNumArray} onViewRulesClick={onViewRulesClick} rulesVisible={rulesVisible}/>
-    <div className={ rulesVisible ? 'rules-outer-container' : 'hidden' }>
-      <div className='rules-inner-container'>
-        <p className='rules-text'>
-          It's you against Mr. Simon! When the game starts, Simon will create a color
-          pattern that grows with each round. Your challenge is to match Simon's pattern
-          by selecting the glowing lights in the correct order.
-        </p>
+      <ScoreBoard roundCount={roundCount} startGame={startGame} playerClickDisplay={playerClickDisplay} randomNumArray={randomNumArray} onViewRulesClick={onViewRulesClick} rulesVisible={rulesVisible}/>
+      <div className={ rulesVisible ? 'rules-outer-container' : 'hidden' }>
+        <div className='rules-inner-container'>
+          <p className='rules-text'>
+            It's you against Mr. Simon! When the game starts, Simon will create a color
+            pattern that grows with each round. Your challenge is to match Simon's pattern
+            by selecting the glowing lights in the correct order.
+          </p>
+        </div>
       </div>
-    </div>
-    <div className={ gameBoardVisible ? 'gameboard-outer-container' : 'hidden' }>
-      <div className='center-circle'>
-        <p className='turn-text-1'>{myOrYours}</p>
-        <p className='turn-text-2'>{turnText}</p>
+      <div className={ gameBoardVisible ? 'gameboard-outer-container' : 'hidden' }>
+        <div className='center-circle'>
+          <p className='turn-text-1'>{myOrYours}</p>
+          <p className='turn-text-2'>{turnText}</p>
+        </div>
+        <div className={ disableButtons ? 'top-section disabled' : 'top-section' }>
+          <div tabindex='0' onClick={() => handlePlayerResponse(0)} className={ redActive ? 'red-box red-active' : 'red-box' }></div>
+          <div tabindex='0' onClick={() => handlePlayerResponse(1)} className={ yellowActive ? 'yellow-box yellow-active' : 'yellow-box' }></div>
+        </div>
+        <div className={ disableButtons ? 'bottom-section disabled' : 'bottom-section' }>
+          <div tabindex='0' onClick={() => handlePlayerResponse(2)} className={ greenActive ? 'green-box green-active' : 'green-box' }></div>
+          <div tabindex='0' onClick={() => handlePlayerResponse(3)} className={ blueActive ? 'blue-box blue-active' : 'blue-box' }></div>
+        </div>
       </div>
-      <div className={ disableButtons ? 'top-section disabled' : 'top-section' }>
-        <div tabindex='0' onClick={() => handlePlayerResponse(0)} className={ redActive ? 'red-box red-active' : 'red-box' }></div>
-        <div tabindex='0' onClick={() => handlePlayerResponse(1)} className={ yellowActive ? 'yellow-box yellow-active' : 'yellow-box' }></div>
-      </div>
-      <div className={ disableButtons ? 'bottom-section disabled' : 'bottom-section' }>
-        <div tabindex='0' onClick={() => handlePlayerResponse(2)} className={ greenActive ? 'green-box green-active' : 'green-box' }></div>
-        <div tabindex='0' onClick={() => handlePlayerResponse(3)} className={ blueActive ? 'blue-box blue-active' : 'blue-box' }></div>
-      </div>
-    </div>
     </section>
   );
 
