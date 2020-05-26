beb7d67c726e01e6f4a9e646ae0b071c3eeaaf9b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-25 18:15:25 -0600

    Print nav headings dynamically

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,19 +4,16 @@ import './Nav.css';
 const Nav = () => {
 
   const performSmoothScroll = sectionName => {
-      document.querySelector(`#${sectionName}-section`).scrollIntoView({
-      behavior: 'smooth'
-    });
+    document.querySelector(`#${sectionName}-section`).scrollIntoView({behavior: 'smooth'});
   }
+
+  const navHeadings = ['home', 'about', 'simon', 'experience', 'projects', 'contact']
 
   return (
     <nav className='nav-outer-container'>
-      <p onClick={() => performSmoothScroll('header')} className='nav-links'>Home</p>
-      <p onClick={() => performSmoothScroll('about')} className='nav-links'>About</p>
-      <p onClick={() => performSmoothScroll('simon-game')} className='nav-links'>Simon</p>
-      <p onClick={() => performSmoothScroll('work-experience')} className='nav-links'>Experience</p>
-      <p onClick={() => performSmoothScroll('projects')} className='nav-links'>Projects</p>
-      <p onClick={() => performSmoothScroll('contact')} className='nav-links'>Contact</p>
+      {navHeadings.map(heading => {
+        return <p onClick={() => performSmoothScroll(`${heading}`)} className='nav-links'>{`${heading}`}</p>
+      })}
     </nav>
   );
 }
