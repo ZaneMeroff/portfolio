8526a619c900360e98cb1d2983cec4809b0fda3d
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-26 10:03:06 -0600

    Plug in components

--- src/components/SimonGame/SimonGame.js
+++ src/components/SimonGame/SimonGame.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import GameBoard from '../GameBoard/GameBoard';
 import './SimonGame.css';
 
-const SimonGame = () => {
-
+const Simon = () => {
   return (
     <section id='simon-section' className='simon-game-outer-container'>
-      <h1>SIMON GAME</h1>
+      <h1 className='simon-title'>Mr. Simon Game</h1>
+      <GameBoard />
     </section>
-  );
-
+  )
 }
 
-export default SimonGame;
+export default Simon;
