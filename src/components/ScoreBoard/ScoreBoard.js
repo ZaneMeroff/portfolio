0c803a267380107396992856cc8be5a665846d20
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-26 10:02:52 -0600

    Add functional code

---
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ScoreBoard.css';
+
+const ScoreBoard = props => {
+
+  return (
+    <div className="scoreboard-outer-container">
+      <div className='round-moves-container'>
+        <h3 className='round-display'>ROUND: {props.roundCount}</h3>
+        <h3 className='round-display'>MOVES: {props.playerClickDisplay}/{props.roundCount}</h3>
+      </div>
+      <div className='score-board-button-container'>
+        <button onClick={props.startGame} className='new-game-button'>{props.randomNumArray.length !== 0 ? 'NEW GAME' : 'START GAME'}</button>
+        <button onClick={props.onViewRulesClick} className='new-game-button'>{props.rulesVisible ? 'HIDE RULES' : 'VIEW RULES'}</button>
+      </div>
+    </div>
+  );
+
+}
+
+export default ScoreBoard;
