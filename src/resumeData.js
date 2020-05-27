cf2cfdcf9530f83dee3922ed17de8de16390ec61
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 16:56:34 -0600

    Add Adrift data to resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -11,5 +11,32 @@ export const resumeData = {
       'Commited 1,200 hours building applications with front end and back end technologies',
       'Dedicated 600 hours working strictly with React, React Native, Router, and Redux'
     ]
-  }
+  },
+  jobs: [
+    {
+      name: 'Adrift Tiki Bar & Restaurant',
+      position: 'General Manager',
+      time: 'March 2016 - March 2019',
+      description: [
+        'Sole general manager responsible for overseeing two million dollar gross annual operation',
+        'Reduced annual labor costs by 8% within first year',
+        'Increased annual gross sales by 15% within first year',
+        'Created multiple successful marketing campaigns and special events',
+        'Managed social media accounts to increase brand awareness',
+        'Created SOPs for all positions and comprehensive onboard training programs'
+      ]
+    },
+    {
+      name: '',
+      position: '',
+      time: '',
+      description: [
+        '',
+        '',
+        '',
+        '',
+        '',
+      ]
+    }
+  ]
 }
