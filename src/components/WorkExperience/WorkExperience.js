dd30808309aa82ccce8165bce8f050d2a53c422c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 16:41:49 -0600

    Refactor structure

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -28,7 +28,9 @@ const WorkExperience = () => {
             <h3 className='experience-heading'>Experience</h3>
           </div>
         </div>
+
         <div className='experience-right-container'>
+
           <h1 className='experience-title'>Adrift Tiki Bar & Restaurant</h1>
           <i className='experience-position'>General Manager</i>
           <p className='experience-time'>March 2016 - March 2019</p>
@@ -40,7 +42,21 @@ const WorkExperience = () => {
             <li className='experience-item'>Managed social media accounts to increase brand awareness</li>
             <li className='experience-item'>Created SOPs for all positions and comprehensive onboard training programs</li>
           </ul>
+
+          <h1 className='experience-title'>Adrift Tiki Bar & Restaurant</h1>
+          <i className='experience-position'>General Manager</i>
+          <p className='experience-time'>March 2016 - March 2019</p>
+          <ul>
+            <li className='experience-item'>Sole general manager responsible for overseeing two million dollar gross annual operation</li>
+            <li className='experience-item'>Reduced annual labor costs by 8% within first year</li>
+            <li className='experience-item'>Increased annual gross sales by 15% within first year</li>
+            <li className='experience-item'>Created multiple successful marketing campaigns and special events</li>
+            <li className='experience-item'>Managed social media accounts to increase brand awareness</li>
+            <li className='experience-item'>Created SOPs for all positions and comprehensive onboard training programs</li>
+          </ul>
+
         </div>
+
       </div>
 
     </section>
