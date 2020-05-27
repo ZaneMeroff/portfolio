321179755b2cabff64894a2f6c211d6b40836e0c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 16:07:36 -0600

    Add resumeData object literal

---
+++ src/resumeData.js
@@ -0,0 +1,15 @@
+export const resumeData = {
+  education: {
+    school: 'Turing School of Software & Design',
+    position: 'Graduate of Front End Development Program',
+    time: 'September 2019 - April 2020',
+    description: [
+      'Built 14 unique web applications from scratch with vanilla JS, React, & React Native',
+      'Created thorough unit testing suites for every application',
+      'Programmed for team projects of four people, paired projects, and solo projects',
+      'Focused on JavaScript fundamentals for 12 weeks',
+      'Commited 1,200 hours building applications with front end and back end technologies',
+      'Dedicated 600 hours working strictly with React, React Native, Router, and Redux'
+    ]
+  }
+}
