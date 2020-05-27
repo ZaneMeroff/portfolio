4887cec57ca63cd3ce876bdcdee607911193abaa
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 13:49:23 -0600

    Add className

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -5,18 +5,22 @@ const WorkExperience = () => {
 
   return (
     <section id='experience-section' className='work-experience-outer-container'>
-      <h3 className='experience-heading'>Education</h3>
+      <div className='experience-heading-box'>
+        <div className='experience-heading-border'>
+          <h3 className='experience-heading'>Education</h3>
+        </div>
+      </div>
       <div className='experience-right-container'>
         <h1 className='experience-title'>Turing School of Software & Design</h1>
-        <p className='experience-position'>Graduate of Front End Development Program</p>
+        <p className='experience-position'><i>Graduate of Front End Development Program</i></p>
         <p className='experience-time'>September 2019 - April 2020</p>
         <ul>
-          <li>Built 14 unique web applications from scratch with vanilla JS, React, & React Native</li>
-          <li>Created thorough unit testing suites for every application</li>
-          <li>Programmed for team projects of four people, paired projects, and solo projects</li>
-          <li>Focused on JavaScript fundementals for 12 weeks</li>
-          <li>Commited 1,200 hours building applications with front end and back end technologies</li>
-          <li>Dedicated 600 hours working strictly with React, React Native, Router, and Redux</li>
+          <li className='experience-item'>Built 14 unique web applications from scratch with vanilla JS, React, & React Native</li>
+          <li className='experience-item'>Created thorough unit testing suites for every application</li>
+          <li className='experience-item'>Programmed for team projects of four people, paired projects, and solo projects</li>
+          <li className='experience-item'>Focused on JavaScript fundementals for 12 weeks</li>
+          <li className='experience-item'>Commited 1,200 hours building applications with front end and back end technologies</li>
+          <li className='experience-item'>Dedicated 600 hours working strictly with React, React Native, Router, and Redux</li>
         </ul>
       </div>
     </section>
