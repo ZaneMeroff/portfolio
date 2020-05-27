6c7bb4f81df42e575e5e462bdb5ee559d94a66a7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 14:05:42 -0600

    Add className to li

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -12,13 +12,13 @@ const WorkExperience = () => {
       </div>
       <div className='experience-right-container'>
         <h1 className='experience-title'>Turing School of Software & Design</h1>
-        <p className='experience-position'><i>Graduate of Front End Development Program</i></p>
+        <i className='experience-position'>Graduate of Front End Development Program</i>
         <p className='experience-time'>September 2019 - April 2020</p>
         <ul>
           <li className='experience-item'>Built 14 unique web applications from scratch with vanilla JS, React, & React Native</li>
           <li className='experience-item'>Created thorough unit testing suites for every application</li>
           <li className='experience-item'>Programmed for team projects of four people, paired projects, and solo projects</li>
-          <li className='experience-item'>Focused on JavaScript fundementals for 12 weeks</li>
+          <li className='experience-item'>Focused on JavaScript fundamentals for 12 weeks</li>
           <li className='experience-item'>Commited 1,200 hours building applications with front end and back end technologies</li>
           <li className='experience-item'>Dedicated 600 hours working strictly with React, React Native, Router, and Redux</li>
         </ul>
