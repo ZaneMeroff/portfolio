37f28ff891225effc5cddde2275361b51c70d76b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-27 16:30:26 -0600

    Correct tabIndex typo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,7 +12,7 @@ const Nav = () => {
   return (
     <nav className='nav-outer-container'>
       {navHeadings.map(heading => {
-        return <p tabindex='0' key={`${heading}`} onClick={() => performSmoothScroll(`${heading}`)} className='nav-links'>{`${heading}`}</p>
+        return <p tabIndex='0' key={`${heading}`} onClick={() => performSmoothScroll(`${heading}`)} className='nav-links'>{`${heading}`}</p>
       })}
     </nav>
   );
