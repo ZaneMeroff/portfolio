a7c0b38b7dc43d4a7fa2385316a680a2088d8a62
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 10:54:51 -0600

    Add anchor tags to buttons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,12 +8,12 @@ const Projects = () => {
       <div className='all-projects-container'>
         {projectData.map(project => {
           return (
-            <div className='project-container'>
+            <div key={project.name} className='project-container'>
               <h1 className='project-title'>{project.name}</h1>
               <i className='project-techs'>{project.techsUsed}</i>
               <p className='project-description'>{project.description}</p>
-              <button className='project-button'>view repo</button>
-              <button className='project-button'>{project.liveButtonText}</button>
+              <a rel='noopener noreferrer' href={project.repoURL} target='_blank'><button className='project-button'>view repo</button></a>
+              <a rel='noopener noreferrer' href={project.liveLink} target='_blank'><button className='project-button'>{project.liveButtonText}</button></a>
             </div>
           )
         })}
