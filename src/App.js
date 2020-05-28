e57f87dd1d006cff5422235f3d56c13e3b59060d
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 15:48:29 -0600

    Destructure imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Nav from './components/Nav/Nav';
-import Header from './components/Header/Header';
-import About from './components/About/About';
-import SimonGame from './components/SimonGame/SimonGame';
-import WorkExperience from './components/WorkExperience/WorkExperience';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
+import { Nav } from './components/Nav/Nav';
+import { Header } from './components/Header/Header';
+import { About } from './components/About/About';
+import { SimonGame } from './components/SimonGame/SimonGame';
+import { WorkExperience } from './components/WorkExperience/WorkExperience';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
 import './App.css';
 
 const App = () => {
