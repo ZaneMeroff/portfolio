f5f69913bcf6bcadd75a03cd0dd8a3405561b017
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 10:22:35 -0600

    Update GameOn title

--- src/projectData.js
+++ src/projectData.js
@@ -1,9 +1,10 @@
 export const projectData = [
   {
-    name: 'Game On! iOS',
+    name: 'Game On! iOS Mobile App',
     techsUsed: 'React Native • React Navigator • TravisCI',
     description: 'A mobile platform for connecting individuals with pickup games in their communites. Features Google Maps integration. Utilizes TravisCI for continuous inegration. Backend database built in Python with Flask. Unit tested with Jest and Enzyme.',
     repoURL: 'https://github.com/Mod-4-Cross-Pol/FE_game_on',
+    liveButtonText: 'demo video',
     liveLink: 'https://www.youtube.com/watch?v=_E9j8tXz8e4&feature=share&fbclid=IwAR1W86DaTGrS4QtOQ0HuCzJv6_t4FWFpXdG4aa_c1_j_CG7oqXR0uHlgDYA'
   }
 ]
