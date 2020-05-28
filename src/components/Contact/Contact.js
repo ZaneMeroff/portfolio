e78008ee1d24ba9e92dc1b1fbc21dea8028d8fe9
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 12:05:56 -0600

    Import resumeData

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { resumeData } from '../../resumeData';
 import './Contact.css';
 
 const Contact = () => {
 
   return (
     <section id='contact-section' className='contact-outer-container'>
-      <h1>CONTACT ME!</h1>
+      <p>Please feel free to contact me for work, suggestions, or networking!</p>
+      <p className='about-text'>Zachary Nemeroff</p>
+      <p className='about-text'>Denver, CO</p>
+      <a rel='noopener noreferrer' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
+      <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button className='resume-button'>view resume.pdf</button></a>
     </section>
   );
 
