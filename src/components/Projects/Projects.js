d9d5286ee6b1297e7551b2175d405ee1373142b7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 10:50:44 -0600

    Add logic to map over project data

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,19 +3,20 @@ import { projectData } from '../../projectData';
 import './Projects.css';
 
 const Projects = () => {
-
   return (
     <section id='projects-section' className='projects-outer-container'>
       <div className='all-projects-container'>
-
-        <div className='project-container'>
-          <h1 className='project-title'>{projectData[0].name}</h1>
-          <i className='project-techs'>{projectData[0].techsUsed}</i>
-          <p className='project-description'>{projectData[0].description}</p>
-          <button className='project-button'>view repo</button>
-          <button className='project-button'>{projectData[0].liveButtonText}</button>
-        </div>
-
+        {projectData.map(project => {
+          return (
+            <div className='project-container'>
+              <h1 className='project-title'>{project.name}</h1>
+              <i className='project-techs'>{project.techsUsed}</i>
+              <p className='project-description'>{project.description}</p>
+              <button className='project-button'>view repo</button>
+              <button className='project-button'>{project.liveButtonText}</button>
+            </div>
+          )
+        })}
       </div>
     </section>
   );
