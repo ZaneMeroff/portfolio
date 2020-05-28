e7a0590828c597ca372eda52f462e79a66b2d1a8
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 12:23:11 -0600

    Add turingAlumniURL

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,11 +6,12 @@ const Contact = () => {
 
   return (
     <section id='contact-section' className='contact-outer-container'>
-      <p>Please feel free to contact me for work, suggestions, or networking!</p>
+      <i className='contact-message'>Please feel free to contact me for work, suggestions, or networking!</i>
       <p className='about-text'>Zachary Nemeroff</p>
       <p className='about-text'>Denver, CO</p>
       <a rel='noopener noreferrer' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
-      <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button className='resume-button'>view resume.pdf</button></a>
+      <a rel='noopener noreferrer' href={resumeData.turingAlumniURL} target='_blank'><p className='about-text email-text'>Turing School Alumni Page</p></a>
+      <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button className='resume-button-footer'>view resume.pdf</button></a>
     </section>
   );
 
