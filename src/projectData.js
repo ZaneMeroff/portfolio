2eb1e99161cd65922b0a7f6564a56848c66d3123
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-29 16:19:09 -0600

    Update GameOn techsUsed

--- src/projectData.js
+++ src/projectData.js
@@ -1,7 +1,7 @@
 export const projectData = [
   {
     name: 'Game On! iOS Mobile App',
-    techsUsed: 'React Native • React Navigator • TravisCI',
+    techsUsed: 'React Native • React Navigator • Redux • TravisCI',
     description: 'A mobile platform for connecting individuals with pickup games in their communites. Features Google Maps integration. Utilizes TravisCI for continuous inegration. Backend database built in Python with Flask. Unit tested with Jest and Enzyme.',
     repoURL: 'https://github.com/Mod-4-Cross-Pol/FE_game_on',
     liveButtonText: 'demo video',
@@ -10,7 +10,7 @@ export const projectData = [
   {
     name: 'Mr. Simon Game',
     techsUsed: 'ReactJS • CSS • Jest • Enzyme',
-    description: 'A virtual recreation of the classic electronic Milton Bradley game from 1978. All game logic is built with dynamic/reusable code using React Hooks. Jest and Enzyme are used for unit testing.',
+    description: 'A virtual recreation of the 1978 electronic handheld game by Milton Bradley. I built this project for fun shortly after graduating from the Turing School of Software and Design. It is written using ReactJS with Hooks and includes snapshot render tests for each component using Jest with Enzyme.',
     repoURL: 'https://github.com/ZaneMeroff/simon_game',
     liveButtonText: 'play game',
     liveLink: 'https://zanemeroff.github.io/simon_game'
