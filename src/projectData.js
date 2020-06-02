989be94e9a1ff3ed593a047f3315e2f0e86f6dc6
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-02 12:27:13 -0600

    Update content

--- src/projectData.js
+++ src/projectData.js
@@ -2,10 +2,18 @@ export const projectData = [
   {
     name: 'Game On! iOS Mobile App',
     techsUsed: 'React Native, Redux, TravisCI, Python, Flask',
-    description: 'A mobile platform for connecting individuals with pickup games in their communites. Features Google Maps integration. Utilizes TravisCI for continuous inegration. Backend database built in Python with Flask. Tested with Jest and Enzyme. This was a team project created with three other people.',
+    description: 'A mobile platform for connecting individuals with pickup games in their communities. Features Google Maps integration. Utilizes TravisCI for continuous integration. React Navigator is used for screen transitions and to manage navigation history. Backend database built in Python with Flask. Component render tests written with Jest and Enzyme. This was a team project created in one week with three other teammates.',
     repoURL: 'https://github.com/Mod-4-Cross-Pol/FE_game_on',
     liveButtonText: 'demo video',
     liveLink: 'https://www.youtube.com/watch?v=_E9j8tXz8e4&feature=share&fbclid=IwAR1W86DaTGrS4QtOQ0HuCzJv6_t4FWFpXdG4aa_c1_j_CG7oqXR0uHlgDYA'
+  },
+  {
+    name: 'MTV Music Trivia App',
+    techsUsed: 'ReactJS, Redux, Router, Jest, Enzyme',
+    description: 'A mobile friendly music trivia app using Redux for global state management and React Router for dynamic URL routing. Jest and Enzyme are used for 97% testing coverage. All Redux and asynchronous functions are tested with happy and sad paths (62 tests overall). PropType checks are implemented for each variable and function.',
+    repoURL: 'https://github.com/ZaneMeroff/mtv_music_trivia',
+    liveButtonText: 'play game',
+    liveLink: 'https://zanemeroff.github.io/mtv_music_trivia'
   },
   {
     name: 'Mr. Simon Game',
