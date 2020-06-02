90564ed40596a7b6c678ebf3b1041b2554c5aab6
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-02 12:26:37 -0600

    Add ARIA labels

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,11 +17,11 @@ export const Header = () => {
       <h1 className='title-name'>Zachary Nemeroff</h1>
       <h3 className='title-industry'>Software Developer</h3>
       <div className='social-icon-container'>
-        <a rel='noopener noreferrer' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
       </div>
-      <img onClick={performSmoothScroll} alt='down arrow button' tabIndex='0' src={downArrowIcon} className='social-icons down-arrow-button'/>
+      <img onClick={performSmoothScroll} aria-label='scroll down' alt='down arrow button' tabIndex='0' src={downArrowIcon} className='social-icons down-arrow-button'/>
     </section>
   );
 }
