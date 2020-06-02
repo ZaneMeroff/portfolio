1d08034f1466df08b22dd50eeba88abf95a33e34
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-02 12:26:22 -0600

    Add ARIA labels

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,8 +14,8 @@ export const About = () => {
           <h2 className='about-text'>Contact Details</h2>
           <p className='about-text'>Zachary Nemeroff</p>
           <p className='about-text'>Denver, CO</p>
-          <a rel='noopener noreferrer' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
-          <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button className='resume-button'>view resume.pdf</button></a>
+          <a rel='noopener noreferrer' aria-label='send email' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
+          <a rel='noopener noreferrer' aria-label='view resume as PDF' href={resumeData.resumeURL} target='_blank'><button className='resume-button'>view resume.pdf</button></a>
         </div>
       </div>
     </section>
