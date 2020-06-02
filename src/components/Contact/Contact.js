f3fea3fe5b90d7d8981833c2a8616482f84666b4
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-02 12:26:28 -0600

    Add ARIA labels

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,14 +11,14 @@ export const Contact = () => {
       <i className='contact-message'>Please feel free to contact me for work, suggestions, or networking!</i>
       <p className='about-text'>Zachary Nemeroff</p>
       <p className='about-text'>Denver, CO</p>
-      <a rel='noopener noreferrer' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
-      <a rel='noopener noreferrer' href={resumeData.turingAlumniURL} target='_blank'><p className='about-text email-text'>Turing School Alumni Page</p></a>
-      <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button className='resume-button-footer'>view resume.pdf</button></a>
+      <a rel='noopener noreferrer' aria-label='send email' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
+      <a rel='noopener noreferrer' aria-label='Turing alumni portfolio' href={resumeData.turingAlumniURL} target='_blank'><p className='about-text email-text'>Turing School Alumni Page</p></a>
+      <a rel='noopener noreferrer' aria-label='view resume as PDF' href={resumeData.resumeURL} target='_blank'><button className='resume-button-footer'>view resume.pdf</button></a>
 
       <div className='social-icons-container'>
-        <a rel='noopener noreferrer' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
       </div>
 
     </section>
