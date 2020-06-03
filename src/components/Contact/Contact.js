106fd86342f3b497ed94bf4e5936aa9ca7a59d4c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-03 10:40:13 -0600

    Change tabIndex

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,14 +13,12 @@ export const Contact = () => {
       <p className='about-text'>Denver, CO</p>
       <a rel='noopener noreferrer' aria-label='send email' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
       <a rel='noopener noreferrer' aria-label='Turing alumni portfolio' href={resumeData.turingAlumniURL} target='_blank'><p className='about-text email-text'>Turing School Alumni Page</p></a>
-      <a rel='noopener noreferrer' aria-label='view resume as PDF' href={resumeData.resumeURL} target='_blank'><button className='resume-button-footer'>view resume.pdf</button></a>
-
+      <a rel='noopener noreferrer' tabIndex='-1' href={resumeData.resumeURL} target='_blank'><button aria-label='view resume as PDF' className='resume-button-footer'>view resume.pdf</button></a>
       <div className='social-icons-container'>
         <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
         <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
         <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
       </div>
-
     </section>
   );
 
