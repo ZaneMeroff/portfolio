c0a5a0157cf147701c556fdf5fa71fadffa6bc37
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-04 10:20:23 -0600

    Add div for project buttons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,8 +12,10 @@ export const Projects = () => {
               <h1 className='project-title'>{project.name}</h1>
               <i className='project-techs'>{project.techsUsed}</i>
               <p className='project-description'>{project.description}</p>
-              <a rel='noopener noreferrer' tabIndex='-1' aria-label='github project repo' href={project.repoURL} target='_blank'><button className='project-button'>view repo</button></a>
-              <a rel='noopener noreferrer' tabIndex='-1' aria-label='live project' href={project.liveLink} target='_blank'><button className='project-button'>{project.liveButtonText}</button></a>
+              <div className='project-buttons-container'>
+                <a rel='noopener noreferrer' tabIndex='-1' aria-label='github project repo' href={project.repoURL} target='_blank'><button className='project-button'>view repo</button></a>
+                <a rel='noopener noreferrer' tabIndex='-1' aria-label='live project' href={project.liveLink} target='_blank'><button className='project-button'>{project.liveButtonText}</button></a>
+              </div>
             </div>
           )
         })}
