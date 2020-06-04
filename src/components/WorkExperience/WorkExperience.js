33fa5510849faaa976ddd00fc644d72993cc14ee
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-04 10:07:16 -0600

    Change <div> to <ul> tech skills

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -46,8 +46,8 @@ export const WorkExperience = () => {
       <div className='tech-experience-container'>
         { buildExperienceHeading('Tech Skills') }
         <div className='tech-skills-right-container'>
-          <div className='tech-skills-bullets'>{resumeData.techSkillz.slice(0, 7).map(item => <li key={item} className='experience-item'>{item}</li>)}</div>
-          <div className='tech-skills-bullets'>{resumeData.techSkillz.slice(7).map(item => <li key={item} className='experience-item'>{item}</li>)}</div>
+          <ul className='tech-skills-bullets'>{resumeData.techSkillz.slice(0, 7).map(item => <li key={item} className='experience-item'>{item}</li>)}</ul>
+          <ul className='tech-skills-bullets'>{resumeData.techSkillz.slice(7).map(item => <li key={item} className='experience-item'>{item}</li>)}</ul>
         </div>
       </div>
     </section>
