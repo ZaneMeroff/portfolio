422e032ab23ea3fab07869aa10c8233fe2008a5f
ZaneMeroff <zanemeroff@gmail.com>
2020-12-11 13:53:10 -0700

    Updates

---
+++ precache-manifest.74618803267b500f589bce07f271dd26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf9ae490ce92afd850f942be3386ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d328aae5076590a92f",
+    "url": "/static/css/main.5eb8de87.chunk.css"
+  },
+  {
+    "revision": "7f707353b972fdbc302c",
+    "url": "/static/js/2.a3a1dbd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3a1dbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d328aae5076590a92f",
+    "url": "/static/js/main.7f1fecea.chunk.js"
+  },
+  {
+    "revision": "35e5f1e8248fa932c41b",
+    "url": "/static/js/runtime-main.f788fbad.js"
+  },
+  {
+    "revision": "0e1a0128d43a1e98b3ffbad41c32a377",
+    "url": "/static/media/down_arrow_icon.0e1a0128.png"
+  },
+  {
+    "revision": "cb5d93c2c4e7e5ebf2b8cefd065e760a",
+    "url": "/static/media/himalayas_sky.cb5d93c2.jpg"
+  },
+  {
+    "revision": "1b299d2ec5b8d087f9c6e51e67907f8b",
+    "url": "/static/media/profile_pic.1b299d2e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.03a84a1a2bc5659574730c12df4122ad.js"
+  "/precache-manifest.74618803267b500f589bce07f271dd26.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
