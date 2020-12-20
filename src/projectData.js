1ae3598d107609d21444fdf9c22c41dfbe1b423e
ZaneMeroff <zanemeroff@gmail.com>
2020-12-20 10:44:05 -0700

    Refactor button project button text in projectData.js

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,7 @@ export const projectData = [
     techsUsed: "Vue.js . Vuex . Vue Router . Jest",
     description: "This is the most recent project I’ve created to showcase the evolution of my programming skills. The app features clean, reusable code and components built with methods leveraging the single responsibility principle. A simple global Vuex store is integrated with Vue Router for view mapping. Jest is used to accomplish nearly 100% unit testing coverage. The app allows a user to randomly assign groups/teams after selecting a number of desired teams and players.",
     repoURL: "https://github.com/ZaneMeroff/team_gen",
-    liveButtonText: "live app",
+    liveButtonText: "Launch App",
     liveLink: "http://zanemeroff.github.io/team_gen"
   },
   {
@@ -12,7 +12,7 @@ export const projectData = [
     techsUsed: "React Native . Redux . TravisCI . Python . Flask",
     description: "A mobile platform for connecting individuals with pickup games in their communities. Features Google Maps integration. Utilizes TravisCI for continuous integration. React Navigator is used for screen transitions and to manage navigation history. Backend database built in Python with Flask. Component render tests written with Jest and Enzyme. This was a team project created in one week with three other teammates.",
     repoURL: "https://github.com/Mod-4-Cross-Pol/FE_game_on",
-    liveButtonText: "demo video",
+    liveButtonText: "Demo Video",
     liveLink: "https://www.youtube.com/watch?v=_E9j8tXz8e4&feature=share&fbclid=IwAR1W86DaTGrS4QtOQ0HuCzJv6_t4FWFpXdG4aa_c1_j_CG7oqXR0uHlgDYA"
   },
   {
@@ -20,7 +20,7 @@ export const projectData = [
     techsUsed: "React.js . Redux . Router . Jest . Enzyme",
     description: "A mobile friendly music trivia app using Redux for global state management and React Router for dynamic URL routing. Jest and Enzyme are used for 97% testing coverage. All Redux and asynchronous functions are tested with happy and sad paths (62 tests overall). PropTypes are implemented for typechecking each variable and function.",
     repoURL: "https://github.com/ZaneMeroff/mtv_music_trivia",
-    liveButtonText: "play game",
+    liveButtonText: "Launch App",
     liveLink: "https://zanemeroff.github.io/mtv_music_trivia"
   },
   {
@@ -28,7 +28,7 @@ export const projectData = [
     techsUsed: "React.js . CSS . Jest . Enzyme",
     description: "A virtual recreation of the 1978 electronic handheld game by Milton Bradley. I built this project for fun shortly after graduating from the Turing School of Software and Design. It is written using React.js with Hooks and includes snapshot render tests for each component using Jest with Enzyme. Just like this portfolio webiste, this project was created with accessibility in mind and can be enjoyed entirely with just the tab and return key.",
     repoURL: "https://github.com/ZaneMeroff/simon_game",
-    liveButtonText: "play game",
+    liveButtonText: "Launch App",
     liveLink: "https://zanemeroff.github.io/simon_game"
   }
 ]
