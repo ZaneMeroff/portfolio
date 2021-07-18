31ae3c13fc60a11026011429a81a516922b1e3e3
ZaneMeroff <zanemeroff@gmail.com>
2021-07-18 11:21:08 -0600

    feat(About): add tech skills bio link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,22 @@ import { resumeData } from '../../resumeData';
 import './About.css';
 
 export const About = () => {
+
+  const scrollToTechSkillsSection = () => {
+    document.querySelector('#skills-section').scrollIntoView({behavior: 'smooth'});
+  }
+
+  const printBio = () => {
+    const bio = resumeData.bioDescription.split('<skillsLink>')
+    return <p className='about-text'>{bio[0]}<span onClick={scrollToTechSkillsSection} className='tech-skills-link'>see complete list</span>{bio[1]}</p>
+  }
+
   return (
     <section id='about-section' className='about-outer-container'>
       <div className='profile-pic-container'><img alt='zacharys profile' src={profilePic} className='profile-pic'/></div>
       <div className='about-info-container'>
         <h2 className='about-text'>Greetings!</h2>
-        <p className='about-text'>{resumeData.bioDescription}</p>
+        {printBio()}
         <div className='contact-info-container'>
           <h2 className='about-text'>Contact Details</h2>
           <p className='about-text'>Zachary Nemeroff</p>
@@ -20,5 +30,4 @@ export const About = () => {
       </div>
     </section>
   );
-
 }
