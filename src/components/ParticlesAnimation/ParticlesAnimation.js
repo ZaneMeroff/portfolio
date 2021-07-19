e479caedb7350518366895cf62a7ae154bda63fd
ZaneMeroff <zanemeroff@gmail.com>
2021-07-19 09:59:18 -0600

    feat(Particles): create component

---
+++ src/components/ParticlesAnimation/ParticlesAnimation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import './ParticlesAnimation.css';
+
+export const ParticlesAnimation = () => {
+
+  return (
+    <section className='particles-outer-container'>
+     
+     <p>test</p>
+     
+     <Particles />
+
+    </section>
+  );
+}
