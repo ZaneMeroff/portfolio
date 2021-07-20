f1709b19beeb60335792509bd29faa6f0e775c9c
ZaneMeroff <zanemeroff@gmail.com>
2021-07-20 08:25:00 -0600

    refactor(ParticlesAnimation): modify animation

--- src/components/ParticlesAnimation/ParticlesAnimation.js
+++ src/components/ParticlesAnimation/ParticlesAnimation.js
@@ -6,25 +6,48 @@ export const ParticlesAnimation = () => {
 
   return (
     <section className='particles-outer-container'>
-          
+
       <Particles
         className='particles'
         params={{
           particles: {
             number: {
-              value: 50,
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 1000,
+              },
             },
             size: {
               value: 3,
+            },
+            move: {
+              speed: 0.8,
+            },
+            color: {
+              value: '#752082',
+            },
+            opacity: {
+              value: 0.9,
+              anim: {
+                enable: true,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: '#F600FF',
+              opacity: 0.4,
+              width: 2,
             },
           },
           interactivity: {
             events: {
               onhover: {
                 enable: true,
-                mode: 'repulse'
-              }
-            }
+                mode: 'repulse',
+              },
+            },
           }
         }} 
       />
