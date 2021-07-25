ed0b13cdeb225fe95d678453751ca0ea003c335b
ZaneMeroff <zanemeroff@gmail.com>
2021-07-25 11:02:58 -0600

    feat(Header): add mouseleave event to icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React, { useEffect } from 'react';
 import { resumeData } from '../../resumeData';
 import { ParticlesAnimation } from '../ParticlesAnimation/ParticlesAnimation';
 import gitHubIcon from '../../images/github_icon.png';
 import twitterIcon from '../../images/twitter_icon.png';
 import linkedInIcon from '../../images/linkedin_icon.png';
+import './Header.css';
 
 export const Header = () => {
+
+  useEffect(() => {
+    const socialIcons = document.querySelectorAll('.icon');
+    socialIcons.forEach(icon => {
+      icon.addEventListener('mouseleave', e => icon.classList.add('mouse-leave'));
+    })
+  })
 
   const performSmoothScroll = () => {
     document.querySelector('#about-section').scrollIntoView({behavior: 'smooth'});
@@ -19,11 +26,11 @@ export const Header = () => {
       <h1 className='title-name'>Nemeroff</h1>
       <h2 className='title-industry'>Software Developer</h2>
       <div className='social-icon-container'>
-        <a rel='noopener noreferrer' aria-label='my github profile' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' aria-label='my linkedin profile' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icons'/></a>
-        <a rel='noopener noreferrer' aria-label='my twitter profile' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icons'/></a>
+        <a rel='noopener noreferrer' aria-label='github profile' href={resumeData.gitHubURL} target='_blank'><img alt='Github icon' src={gitHubIcon} className='social-icon icon'/></a>
+        <a rel='noopener noreferrer' aria-label='linkedin profile' href={resumeData.linkedInURL} target='_blank'><img alt='Linkedin icon' src={linkedInIcon} className='social-icon icon'/></a>
+        <a rel='noopener noreferrer' aria-label='twitter profile' href={resumeData.twitterURL} target='_blank'><img alt='Twitter icon' src={twitterIcon} className='social-icon icon'/></a>
       </div>
-      <button aria-label='scroll down' onClick={performSmoothScroll} className='down-arrow'></button>
+      <button aria-label='scroll down' onClick={performSmoothScroll} className='down-arrow icon'></button>
     </section>
   );
 }
