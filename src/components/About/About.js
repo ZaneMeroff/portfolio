1cccaf220287bd7502a7b20d0847196915ce8e85
ZaneMeroff <zanemeroff@gmail.com>
2021-09-05 09:15:51 -0600

    feat(About): create clip-path div and style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,19 +15,23 @@ export const About = () => {
   }
 
   return (
-    <section id='about-section' className='about-outer-container'>
-      <div className='profile-pic-container'><img alt='zacharys profile' src={profilePic} className='profile-pic'/></div>
-      <div className='about-info-container'>
-        <h2 className='about-text'>Greetings!</h2>
-        {printBio()}
-        <div className='contact-info-container'>
-          <h2 className='about-text'>Contact Details</h2>
-          <p className='about-text'>Zachary Nemeroff</p>
-          <p className='about-text'>Denver, CO</p>
-          <a rel='noopener noreferrer' aria-label='send email' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
-          <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button tabIndex='-1' aria-label='view resume as PDF' className='resume-button'>resume.pdf</button></a>
+    <div>
+      <div className="about-clip-path-top"></div>
+      <section id='about-section' className='about-outer-container'>
+        <div className='profile-pic-container'><img alt='zacharys profile' src={profilePic} className='profile-pic'/></div>
+        <div className='about-info-container'>
+          <h2 className='about-text'>Greetings!</h2>
+          { printBio() }
+          <div className='contact-info-container'>
+            <h2 className='about-text'>Contact Details</h2>
+            <p className='about-text'>Zachary Nemeroff</p>
+            <p className='about-text'>Denver, CO</p>
+            <a rel='noopener noreferrer' aria-label='send email' href='mailto: ZaNemeroff@gmail.com' target='_blank'><p className='about-text email-text'>ZaNemeroff@gmail.com</p></a>
+            <a rel='noopener noreferrer' href={resumeData.resumeURL} target='_blank'><button tabIndex='-1' aria-label='view resume as PDF' className='resume-button'>resume.pdf</button></a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <div className="about-clip-path-bottom"></div>
+    </div>
   );
 }
